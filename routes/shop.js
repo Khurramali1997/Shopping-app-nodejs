d7969c6a38f9459ec0ef11189495eacf4575a88d
Khurrum ali <42839704+Khurramali1997@users.noreply.github.com>
2021-12-01 13:51:59 +0530

    Added two template engines pug,ejs for the shoping app

--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  console.log("shop.js", adminData.products);
+  const products = adminData.products;
+  //res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,20 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminRoutes = require("./routes/admin");
+//My routes
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+//When using pug temp engine
+//app.set("view engine", "pug");
+
+//When using ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
+  //res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
+  res
+    .status(404)
+    .render("error", {
+      pageTitle: "404 ERROR",
+      errorCode: 404,
+      message: "Page Not Found!!!",
+    });
 });
 
 app.listen(port, () => {
